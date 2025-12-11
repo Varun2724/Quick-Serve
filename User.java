d71d3cd04cb366dacd1be9e8eb39e5599f386b39
Varun Patil <138959554+Varun2724@users.noreply.github.com>
2025-12-11 08:36:40 +0530

    Fix formatting and improve displayUser method

--- User.java
+++ User.java
@@ -1,28 +1,27 @@
 package QuickServe;
 
-public class User{
+public class User {
     String name;
     long contact;
     String address;
     String email;
     String password;
-     
-    public User(String name, long contact, String address, String email, String password){
-        super();
+    public User(String name, long contact, String address, String email, String password) {
         this.name = name;
         this.contact = contact;
         this.address = address;
         this.email = email;
         this.password = password;
     }
-    public void displayUser(){
+    public void displayUser() {
         System.out.println();
+        System.out.println("---------USER PROFILE---------");
         System.out.println("Username : " + name);
-        System.out.println("Contact : " + contact);
-        System.out.println("Email : " + email);
-        System.out.println("Address : " + address);
-        System.out.println("password : " + password);
-
+        System.out.println("Contact  : " + contact);
+        System.out.println("Email    : " + email);
+        System.out.println("Address  : " + address);
+        System.out.println("Password : " + password);
     }
-   
 }
+
+    
