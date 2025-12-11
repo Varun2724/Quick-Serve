cb6f9bf162c00a3459e44bb686d2231b47416b36
Varun Patil <138959554+Varun2724@users.noreply.github.com>
2025-12-11 08:36:09 +0530

    Refactor Restaurant class to use a menu list

--- Restaurant.java
+++ Restaurant.java
@@ -1,33 +1,47 @@
 package QuickServe;
+
 import java.util.ArrayList;
 
-
 public class Restaurant {
-    
-        String name;
-        String address;
-        long contact;
-        ArrayList<FoodItem> list;
 
-        public Restaurant(String name, String address, long contact, ArrayList<FoodItem> list) {
-            super();
-            this.name = name;
-            this.address = address;
-            this.contact = contact;
-            this.list = addFoodList();
-
+    String name;
+    String address;
+    long contact;
+    ArrayList<FoodItem> menu;
+    public Restaurant(String name, String address, long contact) {
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.menu = new ArrayList<>();
+        addFoodList();
+    }
+    private void addFoodList() {
+        menu.add(new FoodItem("Masala Dosa", "Veg", 80));
+        menu.add(new FoodItem("Paneer Butter Masala", "Veg", 180));
+        menu.add(new FoodItem("Veg Biryani", "Veg", 150));
+        menu.add(new FoodItem("Mushroom Biryani", "Veg", 220));
+        menu.add(new FoodItem("Paneer Tikka", "Veg", 260));
+        menu.add(new FoodItem("Paneer Tandoori", "Veg", 300));
+        menu.add(new FoodItem("Pasta", "Veg", 140));
+    }
+    public void displayRestaurantDetails() {
+        System.out.println("Restaurant : " + name);
+        System.out.println("Address    : " + address);
+        System.out.println("Contact    : " + contact);
+    }
+    public void displayMenu() {
+        System.out.println();
+        System.out.println("--------- MENU --------");
+        for (FoodItem item : menu) {
+            item.displayFoodItem();
         }
-
-        private ArrayList<FoodItem> addFoodList() {
-            ArrayList<FoodItem> list = new ArrayList<>();
-            list.add(new FoodItem("Biryani","Non-veg", 200 ));
-            list.add(new FoodItem("Chicken Tikka","Non-Veg", 300 ));
-            list.add(new FoodItem("Chicken Tandoor","Non-veg", 400));
-            list.add(new FoodItem("Pasta","Veg", 150));
-            return list;
-
-            
+    }
+    public FoodItem findFoodById(int id) {
+        for (FoodItem item : menu) {
+            if (item.getFoodId() == id) {
+                return item;
+            }
         }
-
-    
+        return null;
+    }
 }
